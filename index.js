3c2c91e6ce4a64ee0980e42ce1744cddc24a37fc
Sterling290 <sterling.becvar@gmail.com>
2020-02-21 12:05:12 -0700

    1.express is running 2.multiple routes were made 3.query are working 4.all csv data is working

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-//express server
+//creating a express server
 const express = require('express');
 let app = express()
+//dealing with imported data 
 var cors = require('cors')
 const bodyparser = require('body-parser')
-// const bigFile = require('./questions.csv')
-const Papa = require("papaparse")
 app.use(cors())
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json())
 app.use(express.static(__dirname + './server/src'));
-//______________________________________________________
-//testing rn 
-// const{Pool,Client} = require('pg')
-// const connectionString = 'postgressql://postgres:PostgreSQL 12@localhost:5432/testdb'
-// const client = new Client({
-// connectionString:connectionString
-// })
-// client.connect()
-// client.query('SELECT * from ',(err,res)=>{console.log(err,res)})
+//_____________________________________________________________________________
+//Create connection,login, & password to db
 const{Pool,Client} = require('pg')
 const pool =new Pool({
 user: "postgres",
@@ -26,37 +18,55 @@ database:"testdb",
 password:"1234",
 port: 5432
 })
+
+//_________________________________________________________________________
+//create table in db
+//copy table from csv
+// NOTE: only have to do this once for each table 
+
+//attempt 1 (successful) <- use node  <-works, but isnt super insufficient
 // pool.query("CREATE TABLE social(id INT, product_id VARCHAR, body VARCHAR , date_written VARCHAR, asker_name VARCHAR, asker_email VARCHAR, reported INT, helpful INT)")
-
-
 // pool.query(COPY social(id, product_id, body, date_written, asker_name, asker_email, reported, helpful) 
 // FROM 'C:\tmp\persons.csv' DELIMITER ',' CSV HEADER)
 
-// COPY question
-//  FROM '/Users/sterlingbecvar/Desktop/questions.csv'
+//attempt 2 (successful)<- use in terminal <- pref
+//psql -s testdb
+//CREATE TABLE reviewphotos(id INT,review_id VARCHAR ,url VARCHAR);
+// COPY reviewphotos
+//  FROM '/Users/sterlingbecvar/Desktop/reviews_photos.csv'
 // DELIMITER ',' CSV HEADER;
-
-
-
+//_________________________________________________________________________
+//connect to database
 const connectionString = 'postgressql://postgres:1234@localhost:5432/testdb'
 const client = new Client({
 connectionString:connectionString
 })
 client.connect()
-
-
-
-//______________________________________________________
-// app.use(express.static('index.html'))
-//send something to the page
-// app.get('/', (req, res) => res.send(Papa.parse(bigFile)))
-app.get('/', (req, res) => 
-client.query('SELECT * from question').then((something)=>{return res.send(something)})
+//__________________________________________________________________________
+//create get request for localhost on questions
+ app.get('/questions', (req, res) => 
+client.query("Select *from question where product_id in ('1')").then((something)=>{return res.send(something)})
+)
+//create get request for localhost on answers
+app.get('/answers', (req, res) => 
+client.query("Select *from answer where question_id in ('1')").then((something)=>{return res.send(something)})
 )
 
+//create get request for localhost on photos
+app.get('/reviewphotos', (req, res) => 
+client.query("Select *from reviewphotos where review_id in ('1')").then((something)=>{return res.send(something)}))
 
-
+//                  #################NOTES#####################
+//all
+//SELECT * from question or from answer or from reviewphotos
+//specific string
+//SELECT * from question where product_id in ('1') or any string
+//specific INT
+//SELECT * from question where id=1
+//___________________________________________________________________________
+//express server port
 let port = 1128;
+//localhost:port/(getRequestRoute)
 app.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
---
+++ server/Docker/basics.sql
@@ -0,0 +1,4 @@
+"this might be used for creating a database in a docker  and 
+ storing randomly generate data to test the docker db,
+ but this also might be done using terminal,js(with fs), and a csv file 
+ so in the future this file might be deleted "
